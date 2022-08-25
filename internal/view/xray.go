3afef95b54ecb399a4de14a3ecea9c70f6476422
Pierre Carru <pierre@carru.fr>
2022-08-25 17:00:00 +0200

    quick hack to use Q instead of ESC, todo: make it configurable

--- internal/view/xray.go
+++ internal/view/xray.go
@@ -123,9 +123,9 @@ func (*Xray) SetInstance(string) {}
 
 func (x *Xray) bindKeys() {
 	x.Actions().Bulk(ui.KeyMap{
-		ui.KeySlash:     ui.NewSharedKeyAction("Filter Mode", x.activateCmd, false),
-		tcell.KeyEscape: ui.NewSharedKeyAction("Filter Reset", x.resetCmd, false),
-		tcell.KeyEnter:  ui.NewKeyAction("Goto", x.gotoCmd, true),
+		ui.KeySlash:    ui.NewSharedKeyAction("Filter Mode", x.activateCmd, false),
+		ui.KeyQ:        ui.NewSharedKeyAction("Filter Reset", x.resetCmd, false),
+		tcell.KeyEnter: ui.NewKeyAction("Goto", x.gotoCmd, true),
 	})
 }
 
